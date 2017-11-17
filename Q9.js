10e3aa96bfb1579f87443bf7e70d0315da2d8c69
kgonzalez452 <kgonzalez@techlaunch.io>
2017-11-16 20:30:30 -0500

    first commit

---
+++ Q9.js
@@ -0,0 +1,11 @@
+// Question 9
+
+// Build a function declaration called maxOf2 that takes in two numbers and
+// returns the greater value, or returns the first value if both of them
+// are equal.
+// Then test your function inside of a console.log by calling it with two
+// different numbers and again in another console.log with two equal numbers.
+function maxOf2(a, b){
+    return (a >= b) ? a : b
+}
+console.log(maxOf2(6, 8));
